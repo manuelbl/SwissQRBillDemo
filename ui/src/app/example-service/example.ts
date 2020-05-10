4e8f808d3137d428bea7d417108ecd59637c202c
Manuel Bl <10954524+manuelbl@users.noreply.github.com>
2020-05-10 12:00:37 +0200

    Initial check-in

---
+++ ui/src/app/example-service/example.ts
@@ -0,0 +1,17 @@
+//
+// Swiss QR Bill Generator
+// Copyright (c) 2018 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+import { QrBill } from '../qrbill-api/qrbill';
+
+export class Example {
+  bill: QrBill;
+  billID: string;
+
+  constructor(bill: QrBill, billID: string) {
+    this.bill = bill;
+    this.billID = billID;
+  }
+}
---
+++ ui/src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+//
+// Swiss QR Bill Generator
+// Copyright (c) 2018 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qrbill-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
